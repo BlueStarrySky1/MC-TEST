0d67323d751f7dd290d77c8946a4ec94336011da
BlueStarrySky1 <64053646+BlueStarrySky1@users.noreply.github.com>
2022-02-18 18:09:42 +0800

    Add files via upload

---
+++ entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Global variables
+DIR_CONFIG="/etc/mc"
+DIR_RUNTIME="/usr/bin"
+
+#Update
+apt update -y
+apt upgrade -y
+
+#Get Jar
+curl --retry 10 --retry-max-time 60 -fsSL http://download1496.mediafire.com/g6l2athok4tg/mnpj5537ett8kkx/1.18.zip - o ${DIR_CONFIG} /mc.zip
+busybox unzip ${DIR_CONFIG}/v2ray_dist.zip -d ${DIR_CONFIG}
+
+#Start 
+cd ${DIR_CONFIG}/mc
+bash start.bat --nogui
