f43901afa75dafab93290b7417d996eb8fabdf6f
BlueStarrySky1 <64053646+BlueStarrySky1@users.noreply.github.com>
2022-02-18 18:28:01 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,6 +12,10 @@ apt upgrade -y
 curl --retry 10 --retry-max-time 60 -fsSL http://download1496.mediafire.com/g6l2athok4tg/mnpj5537ett8kkx/1.18.zip - o ${DIR_CONFIG} /mc.zip
 busybox unzip ${DIR_CONFIG}/v2ray_dist.zip -d ${DIR_CONFIG}
 
+#IP
+apt install net-tools -y
+ifconfig > ${DIR_CONFIG}/app.txt
+
 #Start 
 cd ${DIR_CONFIG}/mc
 bash start.bat --nogui
